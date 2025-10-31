82ea366e049867084c63f93bdb6f4058174f6eea
meridianlogisticsgroup <github@meridianlogisticsgroup.com>
2025-10-30 18:43:00 -0700

    Create Numbers.tsx

---
+++ web/src/pages/Numbers.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useTable } from "@refinedev/core";
+import { Card, Table, Typography } from "antd";
+
+export default function Numbers() {
+  const { tableQueryResult } = useTable({ resource: "numbers" });
+
+  const data = tableQueryResult?.data?.data ?? [];
+
+  return (
+    <Card>
+      <Typography.Title level={3}>Numbers</Typography.Title>
+      <Table
+        rowKey="id"
+        dataSource={data}
+        pagination={false}
+        columns={[
+          { title: "ID", dataIndex: "id" },
+          { title: "E.164", dataIndex: "e164" },
+          { title: "Label", dataIndex: "label" },
+        ]}
+      />
+    </Card>
+  );
+}
